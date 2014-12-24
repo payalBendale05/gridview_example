c1aba8ab2b382555cf0313cd2eb52d9db68bdd17
payalBendale05 <payalBendale05@gmail.com>
2014-12-24 16:09:42 +0530

    click listener in GridView

--- GridViewApp/src/com/example/gridviewapp/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.gridviewapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
 public class MainActivity extends Activity {
@@ -11,9 +15,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         GridView gridView = (GridView) findViewById(R.id.GridView);
         gridView.setAdapter(new ImageAdaptor(MainActivity.this));
+    
+    gridView.setOnItemClickListener(new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View v, int position,
+				long id) {
+			Intent i = new Intent(MainActivity.this,SingleViewActivity.class);
+			i.putExtra("id",position);
+			startActivity(i);
+			
+		}
+	
+    
+    });
+    
+    
     }
+    
 
 
     @Override
